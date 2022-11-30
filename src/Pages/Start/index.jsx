a280a4757ca0c0e7bffa39fa3fdc61ac802a118b
Thiago Souza <thiagosou@gmail.com>
2022-11-30 19:09:36 -0300

    Estilização da página Start

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,20 +1,43 @@
 import React from "react";
-import {View, ScrollView, Text, Image} from "react-native";
+import {View, Text, Image, ScrollView, StyleSheet } from "react-native";
 
 
 export default function Start(){
     return(
-        <View>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View>
-                    <Image source={require("../../assets/icons/logo3.png")} />    
-                    <Text>
-                       Vamos transformar sua vida {"/n"} em jogo, buscando sempre {"/n"} 
+        <View style={styles.container}>
+            <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+                <View style={{ alignItems: "center" }}>
+                    <Image 
+                        source={require("../../assets/icons/logo3.png")} 
+                        style={styles.logo}
+                    />    
+                    <Text style={styles.description}>
+                       Vamos transformar sua vida {"\n"}em um jogo, buscando sempre {"\n"} 
                        o melhor nível.
                     </Text>
                 </View>
             </ScrollView>
         </View>
     );
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "rgba(21, 21, 21, 0.98)"
+    },
+
+    logo: {
+        width: 300,
+        height: 60,
+        marginTop: 60,
+        marginBottom:20,
+    },
+    description: {
+        color: "#FFFFFF",
+        fontSize: 20,
+        textAlign: "center",
+        marginVertical: 60,
+    },
+
+});
