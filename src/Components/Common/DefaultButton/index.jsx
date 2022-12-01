885c217090dda6798d451d438defcdc978b1b142
Thiago Souza <thiagosou@gmail.com>
2022-11-30 22:13:35 -0300

    Inserindo botão padrão com os estilos

---
+++ src/Components/Common/DefaultButton/index.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+export default function DefaultButton({
+    buttonText,
+    handlePress,
+    width,
+    height,
+}) {
+
+    return (
+        <TouchableOpacity 
+        style={[styles.button, {width: width, height: height }]}
+        activeOpacity={0.7} 
+        onPress={handlePress}
+        >
+            <Text> {buttonText}</Text>
+        </TouchableOpacity>
+
+    );
+}
+
+const styles =  StyleSheet.create({
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        borderWidth: 1,
+        borderColor: "#FFFFFF",
+        borderRadius: 10,
+        borderBottom: 20,
+    },
+
+    buttonText: {
+        color: "#FFFFFF",
+        fontWeight: "bold",
+        fontSize: 20,
+    },
+
+});
--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import {View, Text, Image, ScrollView, StyleSheet } from "react-native";
+import DefaultButton from "../../Components/Common/DefaultButton";
+import { useNavigation } from "@react-navigation/native";
 
 import LifeStatus from "../../Components/Common/LifeStatus";
 
 
 export default function Start(){
+
+    const handleNavAppExplanation = () => {
+        console.log("Testando o clique")
+    };
+
     return(
         <View style={styles.container}>
             <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
@@ -18,6 +25,14 @@ export default function Start(){
                        Vamos transformar sua vida {"\n"}em um jogo, buscando sempre {"\n"} 
                        o melhor nível.
                     </Text>
+
+                    <DefaultButton
+                        buttonText={"Continuar"}
+                        handlePress={handleNavAppExplanation}
+                        width= {250}
+                        height= {50}
+                        />
+
                 </View>
             </ScrollView>
         </View>
